62508ad1f62cc3c12148ac6122997226a7eb8e43
Brian Duong <brianminhduong@gmail.com>
2021-12-13 05:12:14 +0000

    Add character limit updater

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+// const eventFired = () => console.log(this);
+$(document).ready(function() {
+  const tweetText = document.querySelector('.new-tweet textarea');
+  const charLimit = 140;
+
+  tweetText.addEventListener("input", function() {
+    let tweetLength = this.value.length;
+    let currentLimit = charLimit - tweetLength;
+    let characterLimit = $(this).next().children(".counter");
+
+    characterLimit[0]["value"] = currentLimit;
+
+    if (characterLimit[0]["value"] < 0) {
+      characterLimit.css('color', 'red');
+    } else {
+      characterLimit.css('color', '');
+    }
+  });
+});
