8b66e1b18b0fca880e537ad33e5812aedf6c296d
Brian Duong <brianminhduong@gmail.com>
2022-01-02 05:48:04 +0000

    Add GET and FETCH methods using jQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,32 +5,6 @@
  */
 
 $(document).ready(function () {
-  //Test Code
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
-
   //Create new element using tweetData object - Helper function
   const createTweetElement = function (tweet) {
     let newTweet = `
@@ -66,15 +40,23 @@ $(document).ready(function () {
     }
   };
 
-  //Creates tweets
-  renderTweets(data);
-
   //Prevent form submission
   $("form").submit(function (event) {
     event.preventDefault();
-    console.log($(this).serialize());
     $.post("/tweets", $(this).serialize());
   });
 
-  // const postTweet = document.querySelector(".tweet-footer button");
+  //Fetches tweets from the http://localhost:8080/tweets page
+  const loadTweets = function () {
+    console.log("Button clicked, performing ajax call...");
+    $.get("/tweets", function (data) {
+      renderTweets(data);
+    });
+  };
+
+  //Loads tweets every time button is clicked
+  $("button").on("click", function () {
+    console.log("Button clicked, performing ajax call...");
+    $.get("/tweets", loadTweets);
+  });
 });
